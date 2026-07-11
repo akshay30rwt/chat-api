bfa337c4b720c69acb8673e85b1ba3ee166bd241
akshay30rwt <akshayrwt8700@gmail.com>
2026-07-11 12:35:12 +0530

    feat: manually create a common http server for express routes and socket.io conncections

--- server.js
+++ server.js
@@ -1,11 +1,24 @@
 require('dotenv').config();
+const http = require('http');
+const { Server } = require('socket.io');
 const app = require('./src/app');
 const connectDB = require('./src/config/db');
+const initSocket = require('./src/sockets/chatSocket');
 
 const PORT = process.env.PORT || 3000;
 
+const server = http.createServer(app);
+
+const io = new Server(server, {
+    cors: {
+        origin: '*'
+    }
+});
+
+initSocket(io);
+
 connectDB().then(() => {
-    app.listen(PORT, () => {
+    server.listen(PORT, () => {
         console.log(`Server running on port: ${PORT}`);
     });
 });
